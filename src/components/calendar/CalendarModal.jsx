178ddb66b02600dd6e2bafe5f6c4035d5fd98ac9
Roberto <rcastellong98@gmail.com>
2023-03-21 23:22:02 +0100

    [add] form validations

--- src/components/calendar/CalendarModal.jsx
+++ src/components/calendar/CalendarModal.jsx
@@ -2,6 +2,7 @@ import moment from 'moment/moment';
 import React, { useState } from 'react';
 import DateTimePicker from 'react-datetime-picker';
 import Modal from 'react-modal';
+import Swal from 'sweetalert2';
 
 const customStyles = {
   content: {
@@ -22,20 +23,72 @@ const nowPlusOne = now.clone().add(1, 'hours');
 const CalendarModal = () => {
   const [dateStart, setDateStart] = useState(now.toDate());
   const [dateEnd, setDateEnd] = useState(nowPlusOne.toDate());
+  const [titleValid, setTitleValid] = useState(true);
+
+  const [formValues, setFormValues] = useState({
+    title: 'Evento',
+    notes: '',
+    start: now.toDate(),
+    end: nowPlusOne.toDate(),
+  });
+
+  const closeModal = () => {
+    /* TODO: Cerrar Modal */
+  };
+
+  const { title, notes, start, end } = formValues;
+
+  const handleInputChanges = ({ target }) => {
+    setFormValues({
+      ...formValues,
+      [target.name]: target.value,
+    });
+  };
 
   const handleStartDateChange = (e) => {
     setDateStart(e);
+    setFormValues({
+      ...formValues,
+      start: e,
+    });
   };
 
   const handleEndDateChange = (e) => {
     setDateEnd(e);
+    setFormValues({
+      ...formValues,
+      end: e,
+    });
+  };
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+
+    const momentStart = moment(start);
+    const momentEnd = moment(end);
+
+    if (momentStart.isSameOrAfter(momentEnd)) {
+      return Swal.fire('Error', 'La fecha fin debe ser mayor que la fecha de inicio', 'error');
+    }
+
+    if (title.trim().length < 2) {
+      setTitleValid(false);
+      return;
+    }
+
+    /* TODO Realizar grabación en Base de datos */
+
+    setTitleValid(true);
+    closeModal();
+
+    console.log(formValues);
   };
 
   return (
     <Modal isOpen={true} style={customStyles} closeTimeoutMS={200} className={'modal'} overlayClassName={'modal-fondo'}>
       <h1> Nuevo evento </h1>
       <hr />
-      <form className='container'>
+      <form className='container' onSubmit={handleSubmitForm}>
         <div className='form-group mb-2'>
           <label>Fecha y hora inicio</label>
           <DateTimePicker className={'form-control'} onChange={handleStartDateChange} value={dateStart} />
@@ -54,14 +107,30 @@ const CalendarModal = () => {
         <hr />
         <div className='form-group mb-2'>
           <label>Titulo y notas</label>
-          <input type='text' className='form-control' placeholder='Título del evento' name='title' autoComplete='off' />
+          <input
+            type='text'
+            className={`form-control ${titleValid ? 'is-valid' : 'is-invalid'}`}
+            placeholder='Título del evento'
+            name='title'
+            autoComplete='off'
+            value={title}
+            onChange={handleInputChanges}
+          />
           <small id='emailHelp' className='form-text text-muted'>
             Una descripción corta
           </small>
         </div>
 
         <div className='form-group mb-2'>
-          <textarea type='text' className='form-control' placeholder='Notas' rows='5' name='notes'></textarea>
+          <textarea
+            type='text'
+            className='form-control'
+            placeholder='Notas'
+            rows='5'
+            name='notes'
+            value={notes}
+            onChange={handleInputChanges}
+          ></textarea>
           <small id='emailHelp' className='form-text text-muted'>
             Información adicional
           </small>
